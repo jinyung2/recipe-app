1f77c62dc191175471899281330a888eba6b7a4d
Jin Choi <jinyoungchoi118@gmail.com>
2020-02-25 18:13:30 -0800

    stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
+import {isPlatformBrowser} from '@angular/common';
 import {AuthService} from './auth/auth.service';
 import {LoggingService} from './logging.service';
 import {Store} from '@ngrx/store';
@@ -13,10 +14,14 @@ import * as AuthActions from './auth/store/auth.actions';
 })
 export class AppComponent implements OnInit {
   constructor(private store: Store<fromApp.AppState>,
-              private loggingService: LoggingService) {
+              private loggingService: LoggingService,
+              @Inject(PLATFORM_ID) private platformId) {
   }
   ngOnInit(): void {
-    this.store.dispatch(AuthActions.autoLogin());
+    if (isPlatformBrowser(this.platformId)) {
+      // only dispatch the loging if we are in the browser
+      this.store.dispatch(AuthActions.autoLogin());
+    }
     this.loggingService.printLog('HELLO FROM AppComponent NgOnIt');
   }
 
